e34709330f45edc085b06e460a5b2996effbd1fe
Judicael <djmx972@gmail.com>
2020-08-23 23:43:38 +0200

    Update UserRepository.mjs

--- src/repository/UserRepository.mjs
+++ src/repository/UserRepository.mjs
@@ -1,4 +1,4 @@
-import rethinkdb from '../config/db/customerAuthenticationRefthinkdb.mjs'
+import { client, cassandra } from '../config/db/cassandra.mjs'
 export default class UserRepository {
     constructor() {
         console.log(rethinkdb);
@@ -12,7 +12,7 @@ export default class UserRepository {
             if (err) throw err;
             console.log(JSON.stringify(result));
         });
-    ;
+        ;
     };
 
     async getUsers() {
@@ -20,24 +20,66 @@ export default class UserRepository {
     };
 
     async getUserByID(id) {
+        const getByUserID = 'SELECT * FROM users WHERE user_id = ?';
+
+        const result = await client.execute(getByUserID, [id])
+            .catch((error) => {
+                console.log(error);
+                
+            });
+        console.log(result.rows[0]);
+
+        const user = {
+            username: result.rows[0].username,
+            email: result.rows[0].email,
+            name: result.rows[0].name
+        }
+
+        return user;
+    };
+
+    async getUserByUsername(username) {
+
+        const getByUsername = 'SELECT * FROM users WHERE username = ?';
+
+        const result = await client.execute(getByUsername, [username])
+            .catch((error) => {
+                console.log(error);
+              
+            });
+
+        const user = {
+            username: result.rows[0].username,
+            email: result.rows[0].email,
+            name: result.rows[0].name
+        }
+
+        return user;
+    };
+
+    async getUserByEmail(email) {
+        const getByUsername = 'SELECT * FROM users WHERE email = ?';
+
+        const result = await client.execute(getByUsername, [email])
+            .catch((error) => {
+                console.log(error);
+               
+            });
+
+        const user = {
+            username: result.rows[0].username,
+            email: result.rows[0].email,
+            name: result.rows[0].name
+        }
+
+        return user;
+    };
+
+    async editUserByID(id, user) {
         if (!id) return;
         return this.usersCollection.filter(
             { id }
-        );
-};
-
-async getUserByEmail(email) {
-    if (!email) return;
-    return this.usersCollection.filter({
-        email
-    });
-};
-
-async editUserByID(id, user) {
-    if (!id) return;
-    return this.usersCollection.filter(
-        { id }
-    ).update(user)
-};
+        ).update(user)
+    };
 
 }
